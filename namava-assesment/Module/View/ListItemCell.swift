188d02f30c6a3d901acc16b75d3e3d5668f66774
Vahid Ghanbarpour <vahidgr.vahid@gmail.com>
2022-10-25 19:15:11 +0330

    add collection view cell item

---
+++ namava-assesment/Module/View/ListItemCell.swift
@@ -0,0 +1,25 @@
+//
+//  ListItemCell.swift
+//  namava-assesment
+//
+//  Created by Vahid Ghanbarpour on 10/25/22.
+//
+
+import UIKit
+
+class ListItemCell: UICollectionViewCell {
+    
+    weak var titleLabel: UILabel!
+    weak var imageView: UIImageView!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+    }
+    
+}
