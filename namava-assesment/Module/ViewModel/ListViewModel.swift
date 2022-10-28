8f5d32af204dca5ce6cf46c00f6ff54bbdac71a7
Vahid Ghanbarpour <vahidgr.vahid@gmail.com>
2022-10-28 06:18:36 +0330

    add a list viewmodel to fetch items from a search api

--- namava-assesment/Module/ViewModel/ListViewModel.swift
+++ namava-assesment/Module/ViewModel/ListViewModel.swift
@@ -5,31 +5,39 @@
 //  Created by Vahid Ghanbarpour on 10/25/22.
 //
 
-import Foundation
 import RxSwift
+import RxFlow
+import RxRelay
 
 protocol ListViewModelProtocol {
     associatedtype Item
     var items: PublishSubject<[Item]> { get set }
-    func fetch()
+    func fetch(search: String, page: Int, perPage: Int)
 }
 
-struct ListViewModel: ListViewModelProtocol {
+struct ListViewModel: Stepper, ListViewModelProtocol {
+    typealias Item = VideoListData
+    var items = PublishSubject<[VideoListData]>()
+    var isLoading = BehaviorRelay<Bool>(value: false)
     
-    typealias Item = VideoListItem
-    var items = PublishSubject<[VideoListItem]>()
+    var steps = PublishRelay<Step>()
     
-    func fetch() {
+    func fetch(search: String, page: Int = 1, perPage: Int = 6) {
+        isLoading.accept(true)
         Task {
-            let response = try await Webservice().load(VideoListItem.resource())
+            let response = try await Webservice().load(VideoList.resource(for: search, page: "\(page)", perPage: "\(perPage)"))
+            isLoading.accept(false)
             switch response {
             case .success(let items):
-                self.items.onNext(items)
-                self.items.onCompleted()
+                self.items.onNext(items.data ?? [])
             case .failure(let error):
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    func pick (video: VideoListData) {
+        self.steps.accept(AppStep.showDetails(video: video))
     }
     
 }
