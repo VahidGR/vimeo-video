04e6ee6d9b32cc1887a154adf11da481927b6421
Vahid Ghanbarpour <vahidgr.vahid@gmail.com>
2022-10-28 06:59:44 +0330

    coordinate app through scene delegate

--- namava-assesment/App/SceneDelegate.swift
+++ namava-assesment/App/SceneDelegate.swift
@@ -6,45 +6,49 @@
 //
 
 import UIKit
+import RxSwift
+import RxFlow
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+class SceneDelegate: UIResponder, UIWindowSceneDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
-
-
+    let bag = DisposeBag()
+    var coordinator = FlowCoordinator()
+    let preferencesService = PreferencesService()
+    
+    lazy var appServices = {
+        return AppServices(preferencesService: self.preferencesService)
+    }()
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        coordinateApp()
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+    
+    private func coordinateApp() {
+        
+        guard let window = window else { return }
+        
+        coordinator.rx.willNavigate.subscribe(onNext: { (flow, step) in
+            print("will navigate to flow=\(flow) and step=\(step)")
+        }).disposed(by: bag)
+        
+        coordinator.rx.didNavigate.subscribe(onNext: { (flow, step) in
+            print("did navigate to flow=\(flow) and step=\(step)")
+        }).disposed(by: bag)
+        
+        let appFlow = Coordinator()
+        
+        coordinator.coordinate(flow: appFlow, with: AppStepper(withServices: appServices))
+        
+        Flows.use(appFlow, when: .created) { root in
+            window.rootViewController = root
+            window.makeKeyAndVisible()
+        }
+        
+        UNUserNotificationCenter.current().delegate = self
+        
     }
 
 
