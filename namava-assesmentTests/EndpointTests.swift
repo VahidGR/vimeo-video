254f85b1351c390a2ea1714b09550694a32d2f21
Vahid Ghanbarpour <vahidgr.vahid@gmail.com>
2022-10-29 12:41:24 +0330

    add endpoint tests

---
+++ namava-assesmentTests/EndpointTests.swift
@@ -0,0 +1,56 @@
+//
+//  EndpointTests.swift
+//  namava-assesmentTests
+//
+//  Created by Vahid Ghanbarpour on 10/29/22.
+//
+
+import XCTest
+
+class EndpointTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testURLRequest() throws {
+        let endpointURL = URL.videoListItems
+        let accountName = "rottingmummy"
+        let endpointToken = "MbyKHQOPJggbujDR"
+        XCTAssertEqual(endpointURL, URL(string: "https://v1.nocodeapi.com/\(accountName)/vimeo/\(endpointToken)/search"))
+    }
+
+    func testModelResource() throws {
+        
+        let query = "query"
+        let page = "1"
+        let perPage = "1"
+        
+        let resource = VideoList.resource(for: query, page: page, perPage: perPage)
+        var components = URLComponents(url: resource.url, resolvingAgainstBaseURL: false)
+        let queryItems = [
+            URLQueryItem(name: "q", value: query),
+            URLQueryItem(name: "page", value: page),
+            URLQueryItem(name: "perPage", value: perPage)
+        ]
+        components?.queryItems = queryItems
+        let url = components?.url
+        let request = URLRequest(url: url!)
+        
+        let accountName = "rottingmummy"
+        let endpointToken = "MbyKHQOPJggbujDR"
+        XCTAssertEqual(request.url, URL(string: "https://v1.nocodeapi.com/\(accountName)/vimeo/\(endpointToken)/search?q=query&page=1&perPage=1"))
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
